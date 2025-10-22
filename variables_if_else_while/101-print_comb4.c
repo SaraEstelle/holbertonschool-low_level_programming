9d50ef7d8bcf445794c2703028d11e29fa506345
SaraEstelle <12152@holbertonstudents.com>
2025-10-22 10:03:45 +0200

    task11

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -7,26 +7,26 @@
  */
 int main(void)
 {
-    int a, b, c;
+	int a, b, c;
 
-    for (a = 0; a < 8; a++)
-    {
-        for (b = a + 1; b < 9; b++)
-        {
-            for (c = b + 1; c < 10; c++)
-            {
-                putchar(a + '0');
-                putchar(b + '0');
-                putchar(c + '0');
+	for (a = 0; a < 8; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for (c = b + 1; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
 
-                if (!(a == 7 && b == 8 && c == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return (0);
+				if (!(a == 7 && b == 8 && c == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
