22cbc20926729c9bc70baf50c37cabcf4d06268f
SaraEstelle <12152@holbertonstudents.com>
2025-10-23 12:07:55 +0200

    task2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
