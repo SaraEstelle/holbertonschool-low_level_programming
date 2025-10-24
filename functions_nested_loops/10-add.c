bbe867b69ac7e004b082fafa06bd4fc847f2d70d
SaraEstelle <12152@holbertonstudents.com>
2025-10-24 15:46:39 +0200

    task9

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that adds two intergers and returns the result
+ * @a : first integer
+ * @b : second integer
+ * Return: sum of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
