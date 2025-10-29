3eff2ec5fc3bc5891f665bbed02b38c563a99d3a
SaraEstelle <12152@holbertonstudents.com>
2025-10-29 13:45:25 +0100

    task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @str : pointer to the string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
