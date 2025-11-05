c937b7ab1768782ec7957d42df375b76c81f2ae6
SaraEstelle <12152@holbertonstudents.com>
2025-11-05 10:40:15 +0100

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ * @argc: number of arguments
+ * @argv: arrays content the past arguments
+ *
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
