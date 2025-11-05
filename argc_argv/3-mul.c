4c28cddda192a79b28dcbfd6985ca79f188c03f7
SaraEstelle <12152@holbertonstudents.com>
2025-11-05 12:47:34 +0100

    task3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,27 @@
-#include"main.h"
+#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - program that multiplies two numbers
- * 
+ *@argc: number of arguments
+ *@argv: arrays of arguments
+ *
+ * Return: 0 (success) or 1 (error)
  */
+int main(int argc, char *argv[])
+{
+	int a, b, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
+	return (0);
+}
