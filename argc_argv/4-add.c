e67df813720b1fc4ae362139de9edb16ca485248
SaraEstelle <12152@holbertonstudents.com>
2025-11-05 13:15:59 +0100

    task4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -12,26 +12,27 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
+
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
-				printf("Error\n"];
-				return(1);)
+				printf("Error\n");
+				return (1);
 			}
 		}
 		sum += atoi(argv[i]);
 	}
-}
-printf("%d\n", sum);
-	return(0);
+
+	printf("%d\n", sum);
+	return (0);
 }
