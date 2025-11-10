5c4b7ff6d39cbe28634c8375392b196b240538d0
SaraEstelle <12152@holbertonstudents.com>
2025-11-10 14:57:50 +0100

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -26,13 +26,13 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2] != '\0')
 		len2++;
 
-	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	concat = malloc(len1 + len2 + 1);
 
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j < len2; j++)
-		concat[i + j] = s2[j];
+		concat[i++] = s2[j];
 
-	concat[i + j] = '\0';
+	concat[i] = '\0';
 	return (concat);
 }
