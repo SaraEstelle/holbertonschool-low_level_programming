ab7af75aa7f7ea213e056fc418a652bca3fdc636
SaraEstelle <12152@holbertonstudents.com>
2025-11-12 13:56:19 +0100

    task3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,7 +17,9 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+/**
+ * dog_t - new name for the type struct dog
+ */
+typedef struct dog dog_t;
 
 #endif /*DOG_H */
