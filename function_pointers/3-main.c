7e5eb3d6df55da265b5ca952f2831bdf6223acb3
SaraEstelle <12152@holbertonstudents.com>
2025-11-17 15:48:42 +0100

    task3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,11 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the entry point of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on failure
+ */
+
