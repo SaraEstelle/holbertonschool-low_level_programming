22120060dba1628626aa0f85a6cbb62c9eed0c3c
SaraEstelle <12152@holbertonstudents.com>
2025-12-01 16:06:38 +0100

    task3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,11 +17,11 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	while (str[len])
 		len++;
-	{
-		new_node = malloc(sizeof(list_t));
-		if (new_node == NULL)
-			return (NULL);
-	}
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->len = len;
 	new_node->next = NULL;
 
