a0e16243bdd2d80546862e6f5cb9981d00cbe7f1
SaraEstelle <12152@holbertonstudents.com>
2025-12-01 14:43:38 +0100

    task1

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * list_len: returns the number of elements in a linked list_t list
+ * list_len - returns the number of elements in a linked list_t list
  * @h: pointer to the list_t list
  *
  * Return: the number of elements in the list
