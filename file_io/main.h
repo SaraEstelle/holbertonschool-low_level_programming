5c66fc12081f0d6a8a7a44fb00d7d8303b8ab443
SaraEstelle <12152@holbertonstudents.com>
2025-12-03 12:41:32 +0100

    task0

---
+++ file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/* Prototypes des fonctions du projet */
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int cp(const char *file_from, const char *file_to); /* Pour la tâche avancée souvent demandée */
+
+#endif
